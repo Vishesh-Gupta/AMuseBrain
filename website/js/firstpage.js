65146dd29f6914b6bbff7eff0348f225b714f0b0
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-09-24 01:43:17 -0400

    first page stuff

--- website/js/firstpage.js
+++ website/js/firstpage.js
@@ -1,59 +1,68 @@
 function displayBlinkDot(element){
   element.css('transform', 'scale(1.0)');
 }
+$('.start').click(function(){
+  isItValidating = true;
+});
   var socket = io();
   var arrayValues = [];
   var scrollTopValue = 0;
   var thisID = '';
   $(document).scroll(function(){
     scrollTopValue = $(this).scrollTop();
-    console.log(scrollTopValue);
   });
   socket.on('yourID', function(doc){
     thisID = doc.thisID
   });
   socket.on('new client', function(doc){
-    console.log(doc.text);
   });
   var current = 100;
   var isItValidating = false;
-  var blinkCounter
+  var blinkCounter = 0;
+  var eyesOpen = true;
 
   socket.on('eyes open', function(doc){
-    if(!isItValidating){
-      $('body').scrollTop(scrollTopValue+=60);
-      scrollTopValue+=60;
-    }else{
-      blinkCounter++;
-      if(blinkCounter == 1){
-        displayBlinkDot($('#firstBlinkFlash'));
-      }else if(blinkCounter == 2){
-        displayBlinkDot($('#secondBlinkFlash'));
-      }
-      else if(blinkCounter == 3){
-        displayBlinkDot($('#thirdBlinkFlash'));
-        socket.emit('this is a pair', {
-          museSocketID:doc.id,
-          userSocketID:thisID
-        });
-      }
-    }
+    console.log('eyes open');
+
 
   });
-  setTimeout(function(){
-    $('body').scrollTop(scrollTopValue+=100);
-  },2000);
-
+  socket.on('eyes close', function(doc){
+    eyesOpen = false;
+  });
   socket.on('challen', function(doc){
+    if(doc.text > 1000){
+      eyesOpen = false;
+    }else{
+      eyesOpen = true;
+    }
     if(arrayValues.length < 150){
       arrayValues.push(doc.text);
     }else{
       arrayValues.push(doc.text);
       arrayValues.shift();
     }
-    console.log(arrayValues);
-    console.log(arrayValues.length);
-  });
-  socket.on('eyes open', function(doc){
-    console.log('eyes open');
+
+    if(!eyesOpen){
+      console.log('!eyesopen');
+      eyesOpen = true;
+      if(!isItValidating){
+        $('body').scrollTop(scrollTopValue+=60);
+        scrollTopValue+=60;
+      }else{
+        blinkCounter++;
+        if(blinkCounter == 1){
+          displayBlinkDot($('#firstBlinkFlash'));
+        }else if(blinkCounter == 2){
+          displayBlinkDot($('#secondBlinkFlash'));
+        }
+        else if(blinkCounter == 3){
+          displayBlinkDot($('#thirdBlinkFlash'));
+          socket.emit('this is a pair', {
+            museSocketID:doc.id,
+            userSocketID:thisID
+          });
+        }
+      }
+    }
+
   });
